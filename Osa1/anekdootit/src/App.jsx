9d3fa41f2ce5bed744a06d87dda22fe938604bf9
alpoaa <aleksi.e.aalto@gmail.com>
2023-09-04 10:36:38 +0300

    anekdootit, step2

--- Osa1/anekdootit/src/App.jsx
+++ Osa1/anekdootit/src/App.jsx
@@ -1,15 +1,28 @@
 import { useState } from 'react'
 
-const Button = ({handleClick, buttonText}) => { 
+const Anecdote = ({anecdote}) => {
   return (
     <div>
-      <button onClick={handleClick}>{buttonText}</button>
+      <h3>{anecdote}</h3>
     </div>
   )
 }
 
+const AnecdoteVote = ({voted}) => {
+  return (
+    <div>
+      <p>Has {voted} votes</p>
+    </div>
+  )
+}
+
+const Button = ({handleClick, buttonText}) => { 
+  return (
+    <button onClick={handleClick}>{buttonText}</button>
+  )
+}
+
 const App = () => {
-  const [selectedAnecdote, setSelectedAnecdote] = useState(0)
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -21,14 +34,25 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
 
+  const [selectedAnecdote, setSelectedAnecdote] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
   const selectRandomAnecdote = () => {
     const randomAnecdote = Math.floor(Math.random() * 8)
     setSelectedAnecdote(randomAnecdote)
   }
 
+  const voteAnecdote = () => {
+    const copyOfVotes = [...votes]
+    copyOfVotes[selectedAnecdote] += 1
+    setVotes(copyOfVotes)
+  }
+
   return (
     <div>
-      <p>{anecdotes[selectedAnecdote]}</p>
+      <Anecdote anecdote={anecdotes[selectedAnecdote]} />
+      <AnecdoteVote voted={votes[selectedAnecdote]} />
+      <Button handleClick={voteAnecdote} buttonText={'vote'} />
       <Button handleClick={selectRandomAnecdote} buttonText={'next anecdote'} />
     </div>
   )
