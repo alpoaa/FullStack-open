f5f1b0f15578a53e4f8a185d194907d697417f2e
alpoaa <aleksi.e.aalto@gmail.com>
2023-12-10 15:49:49 +0200

    kurssitiedot, step10

--- Osa2/kurssitiedot/src/App.jsx
+++ Osa2/kurssitiedot/src/App.jsx
@@ -1,48 +1,4 @@
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header header={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
-const Header = ({header}) => {
-  return (
-    <div>
-      <h3>{header}</h3>
-    </div>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      {parts.map(part => 
-        <Part key={part.id} part={part} />
-        )}
-    </div>
-  )
-}
-
-const Part = ({part}) => {
-  return (
-    <div>
-      <p>{part.name} {part.exercises}</p>
-    </div>
-  )
-}
-
-const Total = ({parts}) => {
-  const countExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return (
-    <div>
-      <h4>Total of exercises: {countExercises}</h4>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
