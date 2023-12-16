97619d1b6daf5a96a175d795ef85d0ecf447d07e
alpoaa <aleksi.e.aalto@gmail.com>
2023-12-16 08:07:01 +0200

    maiden tiedot, initial step

---
+++ Osa2/maidenTiedot/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p>Tasta alkaa</p>
+    </div>
+  )
+}
+
+export default App
