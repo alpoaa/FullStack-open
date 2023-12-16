2b172f32c7121454f0b677970d01a55c3e207799
alpoaa <aleksi.e.aalto@gmail.com>
2023-12-16 23:20:29 +0200

    maiden tiedot, step2

--- Osa2/maidenTiedot/src/components/Country.jsx
+++ Osa2/maidenTiedot/src/components/Country.jsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from 'react'
+import CountryFull from './CountryFull'
+
 const Country = ({country}) => {
+    const [show, setShow] = useState(false)
+
+    const setVisibility = () => {
+        setShow(!show)
+    }
 
     if (country === null) {
         return null
@@ -6,7 +14,8 @@ const Country = ({country}) => {
     
     return (
         <div>
-            <p>{country.name.common}</p>
+            {country.name.common} <button onClick={setVisibility}>{show ? 'hide' : 'show'}</button>
+            {show ? <CountryFull country={country} /> : <div/>}
         </div>
     )
 }
