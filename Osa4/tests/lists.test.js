b880b9a3acbedf26871c87dad8576e47d00f1c90
alpoaa <aleksi.e.aalto@gmail.com>
2024-01-12 14:22:17 +0200

    apufunktioita ja yksikkötestejä, step2

--- Osa4/tests/lists.test.js
+++ Osa4/tests/lists.test.js
@@ -6,3 +6,53 @@ test('dummy returns one', () => {
     const result = listHelper.dummy(blogs)
     expect(result).toBe(1)
 })
+
+describe('total like tests', () => {
+    const oneBlogList = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        }
+      ]
+
+    const multipleBlogsList = [
+        {
+          _id: "5a422a851b54a676234d17f7",
+          title: "React patterns",
+          author: "Michael Chan",
+          url: "https://reactpatterns.com/",
+          likes: 7,
+          __v: 0
+        },
+        {
+          _id: "5a422aa71b54a676234d17f8",
+          title: "Go To Statement Considered Harmful",
+          author: "Edsger W. Dijkstra",
+          url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+          likes: 5,
+          __v: 0
+        },
+        {
+          _id: "5a422b3a1b54a676234d17f9",
+          title: "Canonical string reduction",
+          author: "Edsger W. Dijkstra",
+          url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+          likes: 12,
+          __v: 0
+        }
+      ]
+
+    test('Test of total likes with one blog list', () => {
+        const result = listHelper.totalLikes(oneBlogList)
+        expect(result).toBe(5)
+    })
+
+    test('Test with multiple blogs in list', () => {
+        const result = listHelper.totalLikes(multipleBlogsList)
+        expect(result).toBe(24)
+    })
+})
--- Osa4/utils/list_helper.js
+++ Osa4/utils/list_helper.js
@@ -2,4 +2,8 @@ const dummy = (blogs) => {
     return 1
 }
 
-module.exports = {dummy}
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, {likes}) => sum + likes, 0)
+}
+
+module.exports = {dummy, totalLikes}
