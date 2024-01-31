53488d41174d52a85bf0728cc1a7ae60c2a562af
alpoaa <aleksi.e.aalto@gmail.com>
2024-01-31 07:15:06 +0200

    blogilistan laajennus, step3

--- Osa4/app.js
+++ Osa4/app.js
@@ -3,6 +3,7 @@ const express     = require('express')
 const cors        = require('cors')
 const mongoose    = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const logger      = require('./utils/logger')
 const middleware  = require('./utils/middleware')
 const app         = express()
@@ -22,7 +23,10 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLog)
+
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
