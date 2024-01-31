bde077735d607c62bdbacf909dd17e5621d5fc89
alpoaa <aleksi.e.aalto@gmail.com>
2024-01-31 10:55:59 +0200

    blogilistan laajennus, step7

--- Osa4/controllers/blogs.js
+++ Osa4/controllers/blogs.js
@@ -1,6 +1,15 @@
-const blogsRouter = require('express').Router()
-const Blog        = require('../models/blog')
-const User        = require('../models/user')
+const jsonWebToken = require('jsonwebtoken')
+const blogsRouter  = require('express').Router()
+const Blog         = require('../models/blog')
+const User         = require('../models/user')
+
+const getRequestToken = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 blogsRouter.get('/', async(request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -23,7 +32,17 @@ blogsRouter.get('/:id', async(request, response, next) => {
 
 blogsRouter.post('/', async(request, response, next) => {
     const body = request.body
-    const user = await User.findById(body.userId)
+    const decodedToken = jsonWebToken.verify(getRequestToken(request), process.env.SECRET)
+
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'Invalid token' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    if (!user) {
+        return response.status(401).json({ error: 'User not exists' })
+    }
 
     const newBlog = new Blog({
         title: body.title,
