71bf447b513395564c478d2a7ef90e9b5d609c03
alpoaa <aleksi.e.aalto@gmail.com>
2024-02-16 14:49:20 +0200

    blogilistan testit, step2

--- Osa5/Bloglist-frontend/src/tests/Blog.test.js
+++ Osa5/Bloglist-frontend/src/tests/Blog.test.js
@@ -1,18 +1,47 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import Blog from '../components/Blog'
 import testHelper from '../utils/testHelper'
 
 
 describe('Tests for <Blog /> component', () => {
   test('Rendering blog values', () => {
-    const blog = testHelper.testBlog
-    const user = testHelper.testUser
+    const testBlog = testHelper.testBlog
+    const testUser = testHelper.testUser
 
-    render(<Blog blog={ blog } user={ user } />)
-    const element = screen.getByText(blog.title)
+    render(<Blog blog={ testBlog } user={ testUser } />)
+    const element = screen.getByText(testBlog.title)
 
     expect(element).toBeDefined()
   })
+
+  test('Show all details when button cliked', async() => {
+    const testBlog = testHelper.testBlog
+    const testUser = testHelper.testUser
+
+    const { container } = render(<Blog blog={ testBlog } user={ testUser } />)
+
+    const user             = userEvent.setup()
+    const visibilityButton = screen.getByText('View')
+
+    await user.click(visibilityButton)
+
+    const blogDetailsDiv = container.querySelector('.blogdetails')
+
+    expect(blogDetailsDiv).toBeDefined()
+
+    const urlElement = screen.getByText(testBlog.url)
+    expect(urlElement).toHaveTextContent(testBlog.url)
+
+    const userElement = screen.getByText(testBlog.user.name)
+    expect(userElement).toHaveTextContent(testBlog.user.name)
+
+    const likesElement = screen.getByText(`Likes: ${testBlog.likes}`)
+    expect(likesElement).toHaveTextContent(`Likes: ${testBlog.likes}`)
+
+  })
+
 })
